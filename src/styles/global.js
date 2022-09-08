6a7f48554450dd2c437bdb4fe6f3c4f7007f8cab
Felipe Scola <felipescola0@gmail.com>
2022-09-08 11:28:37 -0300

    Primeiro Commit

---
+++ src/styles/global.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    }
+
+  body {
+    background-color: #f2f2f2;
+    font-family: 'Poppins', sans-serif;
+  }
+`;
+
+export default GlobalStyle;
